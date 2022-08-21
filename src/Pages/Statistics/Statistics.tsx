2d2d38c047537984d275085e2b60751990e90470
Gothica <semidevilish666@gmail.com>
2022-08-21 17:23:13 +0300

    feat:statistic page

--- src/Pages/Statistics/Statistics.tsx
+++ src/Pages/Statistics/Statistics.tsx
@@ -1,11 +1,64 @@
-import Header from '../../Components/Header/Header';
+import './style.scss';
 
 const Statistics = () => {
     return (
-        <>
-            <Header />
-            <div>Statistics</div>
-        </>
+        <div className="statistics_wrapper">
+            <h2>Статистика</h2>
+            <h4>Успехи сегодня</h4>
+            <div className="stat-day">
+                <div className="stat-words">
+                    <h5>По словам</h5>
+                    <div className="stat-words_result">
+                        <div className="stat-words_count">
+                            <p>Новых слов</p>
+                            <p>0 шт</p>
+                        </div>
+                        <div className="stat-words_count">
+                            <p>Изученных слов</p>
+                            <p>0 шт</p>
+                        </div>
+                        <div className="stat-words_count">
+                            <p>Правильных ответов</p>
+                            <p>0 %</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="stat-games">
+                    <h5>Аудиовызов</h5>
+                    <div className="stat-games_result">
+                        <div className="stat-games_count">
+                            <p>Новых слов</p>
+                            <p>0 шт</p>
+                        </div>
+                        <div className="stat-games_count">
+                            <p>Правильных ответов</p>
+                            <p>0%</p>
+                        </div>
+                        <div className="stat-games_count">
+                            <p>Самая длинная серия правильных ответов</p>
+                            <p>0 шт</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="stat-games">
+                    <h5>Спринт</h5>
+                    <div className="stat-games_result">
+                        <div className="stat-games_count">
+                            <p>Новых слов</p>
+                            <p>0 шт</p>
+                        </div>
+                        <div className="stat-games_count">
+                            <p>Правильных ответов</p>
+                            <p> 0%</p>
+                        </div>
+                        <div className="stat-games_count">
+                            <p>Самая длинная серия правильных ответов</p>
+                            <p>0 шт</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
