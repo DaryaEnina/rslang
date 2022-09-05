3f912f4683d2b177e8426b32f0cd56862045deb0
Gothica <semidevilish666@gmail.com>
2022-09-05 22:59:23 +0300

    feat: error page

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -15,24 +15,26 @@ import 'Pages/Login/LogIn.css';
 
 import SprintMain from 'Pages/Games/Sprint/SprintMain';
 import AudiogameMain from 'Pages/Games/Audiogame/AudiogameMain';
+import Error from 'Pages/Errors/Error';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Template />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Autorisation />} />
-        <Route path="/signup" element={<Registration />} />
-        <Route path="/statistics" element={<Statistics />} />
-        <Route path="/games" element={<Games />} />
-        <Route path='/audiogame-main' element={<AudiogameMain />} />
-        <Route path="/audiogame" element={<Audiogame />} />
-        <Route path="/sprint-main" element={<SprintMain />} />
-        <Route path="/sprint" element={<Sprint />} />
-        <Route path="/book" element={<Book />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Template />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<Autorisation />} />
+                <Route path="/signup" element={<Registration />} />
+                <Route path="/statistics" element={<Statistics />} />
+                <Route path="/games" element={<Games />} />
+                <Route path="/audiogame-main" element={<AudiogameMain />} />
+                <Route path="/audiogame" element={<Audiogame />} />
+                <Route path="/sprint-main" element={<SprintMain />} />
+                <Route path="/sprint" element={<Sprint />} />
+                <Route path="/book" element={<Book />} />
+                <Route path="*" element={<Error />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
